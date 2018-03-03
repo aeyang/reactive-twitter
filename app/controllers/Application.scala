095fc4d94fda5ab7ff63d85e642e7db0ed1b1180
Alan Yang <alyang@linkedin.com>
2018-03-02 23:19:33 -0800

    Added Iteratees
      - Using Iteratee to consume stream of data from Twitter
        - Print out body of message by turning bytes into characters
      - Updated wrong API key

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,13 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-import play.api.libs.oauth.{ConsumerKey, RequestToken}
+import play.api.libs.oauth.{ConsumerKey, RequestToken, OAuthCalculator}
 import play.api.Play.current
 import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.ws._
+import play.api.libs.iteratee._
+import play.api.Logger
 
 class Application extends Controller {
 
@@ -16,13 +19,60 @@ class Application extends Controller {
 
   /**
     * Create an Action to render Tweets HTML page
-    * @return
+    * @return Future
     */
-  // Use Action.async to return a Future of a result for the next step
-  def tweets = Action.async{
-    // Retrieve the Twitter credentials from application.conf using a for-yield block
-    // The type of credentials is an Option of a Tuple with two elements in the tuple
-    val credentials: Option[(ConsumerKey, RequestToken)] = for {
+  def tweets = Action.async {
+
+    /*
+    Define a logging iteratee that consumes a stream asynchronously and logs the
+    contents when data is available
+
+    `Iteratee.foreach[E] creates a new iteratee that consumes each input it receives
+    by performing a side-effecting action.
+
+    `foreach` is NOT a method of an iteratee. It is a method of the Iteratee library
+    used to create a "foreach" iteratee.
+
+    Iteratees lets you act on streams of data ASYNCHRONOUSLY. What does this mean?
+    It means that if no data is coming through the stream, the iteratee won't hold onto
+    the thread. Only when new data arrives will the iteratee make use of the thread again.
+    This is in contrast to how java.io.OutputStream works for example.
+    */
+    val loggingIteratee = Iteratee.foreach[Array[Byte]] { array =>
+      Logger.info(array.map(_.toChar).mkString)
+    }
+
+    // credentials is a function call
+    credentials.map {
+      case (consumerKey, requestToken) =>
+        WS
+          .url("https://stream.twitter.com/1.1/statuses/filter.json")
+          .sign(OAuthCalculator(consumerKey, requestToken))
+          .withQueryString("track" -> "Washington") // Specify a query string
+          // Execute HTTP request and retrieves the response as possibly an infinite stream
+          .get{ response =>
+            Logger.info("Status: " + response.status)
+            // Start consuming response
+            loggingIteratee
+          }
+          .map { _ =>
+            Ok("Stream closed") // This only fires when the stream is entirely consumed or closed
+          }
+        } getOrElse {
+          // Return type mandates Future, so return an Error message wrapped in a Future
+          Future.successful {
+            InternalServerError("Twitter credentials missing")
+          }
+      }
+  }
+
+  /**
+   * Method to retrieve Twitter API keys from conf
+   *
+   * Retrieve the Twitter credentials from application.conf using a for-yield block
+   * The type of credentials is an Option of a Tuple with two elements in the tuple
+   */
+  def credentials: Option[(ConsumerKey, RequestToken)] = for {
       apiKey      <- Play.configuration.getString("twitter.apiKey")
       apiSecret   <- Play.configuration.getString("twitter.apiSecret")
       token       <- Play.configuration.getString("twitter.token")
@@ -32,19 +82,4 @@ class Application extends Controller {
       ConsumerKey(apiKey, apiSecret),
       RequestToken(token, tokenSecret)
     )
-
-    credentials.map {
-      case (consumerKey, requestToken) => {
-        // Return type mandates Future, so return a Ok wrapped in a Future
-        Future.successful{
-          Ok
-        }
-      }
-    } getOrElse {
-      // Return type mandates Future, so return an Error message wrapped in a Future
-      Future.successful {
-        InternalServerError("Twitter credentials missing")
-      }
-    }
-  }
 }
--- build.sbt
+++ build.sbt
@@ -18,3 +18,7 @@ resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
 routesGenerator := InjectedRoutesGenerator
+
+// Bug in async-http-client that plagues Play 2.4.x breaks OAuth when request contains characters that
+// need to be encoded.
+libraryDependencies += "com.ning" % "async-http-client" % "1.9.29"
