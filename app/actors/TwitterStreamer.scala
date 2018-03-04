edef3eb0f37a2d2ce2c98aa45c37c0eeea52c79e
Alan Yang <alyang@linkedin.com>
2018-03-03 16:34:40 -0800

    Created a Broadcasting enumerator
      - Before this, Play would create one new actor for each WebSocket
        connection. We should just connect to Twitter once.
        And broadcast the stream to all connections.
    
      - Gave the TwitterStreamer actor subscribe and receive actions to
        subscribe to the Broadcast

--- app/actors/TwitterStreamer.scala
+++ app/actors/TwitterStreamer.scala
@@ -22,7 +22,61 @@ class TwitterStreamer(out: ActorRef) extends Actor {
 }
 
 
+// Companion object of the TwitterStreamer actor
 object TwitterStreamer {
   // Helper method that initializes a new Props object
   def props(out: ActorRef) = Props(new TwitterStreamer(out))
-}
+
+  private var broadcastEnumerator: Option[Enumerator[JsObject]] = None;
+
+  def connect(): Unit = {
+    credentials.map {
+      case (consumerKey, requestToken) =>
+        // Set up joined iteratee/enumerator pair
+        val (iteratee, enumerator) = Concurrent.joined[Array[Byte]]
+        // Set up stream transformation pipeline
+        val jsonStream: Enumerator[JsObject] = enumerator &>
+        Encoding.decode() &>
+        Enumeratee.grouped(JsonIteratees.jsSimpleObject)
+
+        // Initialize the broadcast enumerator using the transformed stream as a source
+        val (be, __) = Concurrent.broadcast(jsonStream)
+        broadcastEnumerator = Some(be_)
+
+        val url = "https://stream.twitter.com/1.1/statuses/filter.json"
+        WS
+          .url(url)
+          .sign(OAuthCalculator(consumerKey, requestToken))
+          .withQueryString("track" -> "Washington")
+          .get {response =>
+            Logger.info("Status: " + response.status)
+            iteratee
+          }.map { _ =>
+            Logger.info("Twitter stream closed")
+          }
+        } getOrElse {
+          Logger.error("Twitter credentials missing")
+        }
+    }
+
+    // This method first checks if a broadcast-Enumerator exists. If not, we establish a connection.
+    // Then, create the twitterClient iteratee which sends each JSON object to the browser using
+    //   the actor reference.
+    def subscribe(out: ActorRef) : Unit = {
+      if (broadcastEnumerator.isEmpty) {
+        connect()
+      }
+      val twitterClient = Iteratee.foreach[JsObject] { t => out ! t }
+      broadcastEnumerator.foreach { enumerator =>
+        enumerator run twitterClient
+      }
+    }
+
+
+    // When a client subscribes, call the subscribe method
+    def receive = {
+      case "subscribe" =>
+        Logger.info("Received subscription from client")
+        TwitterStreamer.subscribe(out)
+    }
+  }
